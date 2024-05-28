aaf967d70a81411fd6aec28d9f492b7ca0b3060c
Thomas <74390441+ThomRib@users.noreply.github.com>
2024-05-28 09:33:59 +0700

    baru

---
+++ database/migrations/2024_05_27_092912_add_username_field_in_users_tables.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //yang baru di tambahkan
+			$table->string('username')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //yang baru di tambahkan
+			$table->dropColumn('username');
+        });
+    }
+};
